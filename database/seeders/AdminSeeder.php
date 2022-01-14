55ca187579972e86f47c0fa746b947705547dd5f
Dasun Tharanga <dasuntharangaflash@gmail.com>
2022-01-15 02:42:45 +0530

    :white_check_mark: Admin seeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'hello@dasun.dev',
+            'password' => bcrypt(12345678),
+            'role' => 'admin'
+        ]);
+    }
+}
